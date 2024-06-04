7d884f7e7b110ae7d21e7c04a2186d672604d50a
Aluno <Aluno@10.85.198.42>
2024-06-04 17:01:01 -0300

    java project galToLit

---
+++ src/galtolit/GalToLit.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package galToLit;
+
+/**
+ *
+ * @author Aluno
+ */
+public class GalToLit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    // declare as variáveis
+    double gallons=10;
+    double liters=0;
+    // adicione o cálculo aqui
+    // exiba o resultado para o usuário
+    System.out.println(gallons+" gallons equals "+liters+" liters");
+
+    }
+    
+}
